d3bd82c98dd161ad4253c2c3dd7f094daa210f9f
BoyChai <1972567225@qq.com>
2024-08-07 17:26:01 +0800

    feat: add jwt and cors middleware

---
+++ controller/middle/cors.go
@@ -0,0 +1,25 @@
+package middle
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
+
+// CORS 防止跨域问题出现
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 设置允许的来源、HTTP方法和请求头
+		c.Header("Access-Control-Allow-Origin", viper.GetString("Settings.HTTP_Allow_Origin"))
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Origin, Accept")
+		c.Header("Access-Control-Allow-Credentials", "true")
+
+		// 处理预检请求
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
