d64226378142e5a34764502da8e55a8b2e1b585b
BoyChai <1972567225@qq.com>
2024-08-07 19:07:56 +0800

    refactor: update main function

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"github.com/BoyChai/Guard/config"
+	"github.com/BoyChai/Guard/controller/http"
+	"github.com/BoyChai/Guard/controller/middle"
+	"github.com/BoyChai/Guard/dao"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
 
 func main() {
-	fmt.Println("Hello, Guard!")
+	// 读取配置
+	config.InitConfig()
+	// 初始化数据库链接
+	dao.InitClient()
+	// 自动加载数据表
+	dao.AutoTables()
+	// 初始化路由
+	g := gin.Default()
+	g.Use(middle.CORS())
+	g.Use(middle.JWTAuth())
+	http.Router.InitApiRouter(g)
+	// 启动
+	g.Run(":" + viper.GetString("Settings.Port"))
 }
