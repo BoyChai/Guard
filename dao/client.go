2091efc9639686bb13b1674e060f2640427e8b46
BoyChai <1972567225@qq.com>
2024-08-07 16:47:09 +0800

    faet: super user hook

--- dao/client.go
+++ dao/client.go
@@ -3,6 +3,7 @@ package dao
 import (
 	"log"
 
+	"github.com/BoyChai/Guard/utils"
 	"github.com/spf13/viper"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -34,5 +35,22 @@ func AutoTables() {
 	err = Dao.db.AutoMigrate(&Card{})
 	if err != nil {
 		log.Fatalln("加载数据表错误 -- Card")
+	}
+	superUserCreate()
+}
+
+// 超级用户维护
+func superUserCreate() {
+	// 检查数据库中是否存在admin用户没有则创建
+	var count int64
+	tx := Dao.db.Model(&User{}).Where("name = ?", "admin").Count(&count)
+	if tx.Error != nil {
+		log.Fatalln("UpserUser检查错误")
+	}
+	if count == 0 {
+		Dao.db.Create(&User{
+			Name: viper.GetString("Settings.SuperUserName"),
+			Pass: utils.CalculateMD5Hash(viper.GetString("Settings.SuperUserPass")),
+		})
 	}
 }
